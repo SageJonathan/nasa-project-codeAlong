ecddecb73224dd588db9d1cf055fea023a34f6d1
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-07-04 20:58:03 -0600

    refracot models

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+
 const path = require('path');
+
 const planetsRouter = require('./routes/planets/planets.router');
 const launchesRouter = require('./routes/launches/launches.router');
 
@@ -14,8 +16,8 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches',launchesRouter);
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'..','public','index.html'));
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,7 +2,10 @@
 
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
+    flightNumber: 100,
     mission: 'kepler Exploration X',
     rocket: 'Explorer Is1',
     launchDate: new Date('December 27, 2030'),
@@ -16,8 +19,19 @@ launches.set(launch.flightNumber, launch);
 
 function getAllLaunches(){
     return Array.from(launches.values())
-}
+};
+
+function addNewLaunch (launch){
+    latestFlightNumber += 100;
+    launches.set(latestFlightNumber, Object.assign(launch,{
+        flightNumber : latestFlightNumber,
+        customers: ['ZTM','NASA'],
+        upcoming: true,
+        success: true,
+    }));
+};
 
 module.exports ={
     getAllLaunches,
+    addNewLaunch
 };

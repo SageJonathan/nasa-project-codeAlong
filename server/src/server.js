94fe1b6100240c782242d4833a2b989a969e5cbb
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-07-15 18:31:19 -0600

    add mongoose connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require("http");
+const mongoose = require('mongoose');
 const app = require("./app");
 
 const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL= 'mongodb+srv://dev:rkDxUkce5rUDejLC@cluster0.ndzj9q2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () =>{
+  console.log('MongoDB Connection Ready')
+});
+
+mongoose.connection.on('error', (error) =>{
+  console.error(error)
+});
+
 async function startServer (){
+  await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
 server.listen(PORT, () => {
